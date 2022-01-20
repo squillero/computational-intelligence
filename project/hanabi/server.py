25963b9f3b024d46168937b23beb5357e56e5de2
Andrea Calabrese <andrea1995.c@live.com>
2022-01-20 20:35:06 +0100

    Avoid delay if server crashes (thanks Giuseppe R)

--- project/hanabi/server.py
+++ project/hanabi/server.py
@@ -134,6 +134,7 @@ def manageInput():
 
 def manageNetwork():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         logging.info("Hanabi server started on " + HOST + ":" + str(PORT))
         while True:
