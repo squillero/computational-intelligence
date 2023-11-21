403fe719f42e0ab3cba26f4683cd4924b981adbd
Giovanni Squillero <squillero@polito.it>
2023-11-21 14:17:49 +0100

    Add LAB2
    Former-commit-id: 3db9df9c9c09b64867b6d4129befe642d6d73437

---
+++ 2023-24/lab2_lib.py
@@ -0,0 +1,36 @@
+# Copyright © 2023 Giovanni Squillero <giovanni.squillero@polito.it>
+# https://github.com/squillero/computational-intelligence
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+from abc import abstractproperty
+
+class AbstractProblem:
+    def __init__(self):
+        self._calls = 0
+
+    @abstractproperty
+    def x(self):
+        pass
+
+    @property
+    def calls(self):
+        return self._calls
+
+    @staticmethod
+    def onemax(genome):
+        return sum(bool(g) for g in genome)
+
+    def __call__(self, genome):
+        self._calls += 1
+        fitnesses = sorted((AbstractProblem.onemax(genome[s::self.x]) for s in range(self.x)), reverse=True)
+        val = fitnesses[0] - sum(f*(.1 ** (k+1)) for k, f in enumerate(fitnesses[1:]))
+        return val / len(genome) * self.x
+
+def make_problem(a):
+    class Problem(AbstractProblem):
+        @abstractproperty
+        def x(self):
+            return a
+
+    return Problem()
+
