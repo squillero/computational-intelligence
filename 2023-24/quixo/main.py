b9bfd0b6aa3ecce71e208bf79000ec88fd0c0e08
Andrea Calabrese <andrea1995.c@live.com>
2023-12-07 14:25:10 +0100

    Updated quixo

--- 2023-24/quixo/main.py
+++ 2023-24/quixo/main.py
@@ -12,10 +12,20 @@ class RandomPlayer(Player):
         return from_pos, move
 
 
+class MyPlayer(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
+        from_pos = (random.randint(0, 4), random.randint(0, 4))
+        move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
+        return from_pos, move
+
+
 if __name__ == '__main__':
     g = Game()
     g.print()
-    player1 = RandomPlayer()
+    player1 = MyPlayer()
     player2 = RandomPlayer()
     winner = g.play(player1, player2)
     g.print()
