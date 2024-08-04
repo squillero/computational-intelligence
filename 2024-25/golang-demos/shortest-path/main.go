12f602938b7f1f8dfbb73b2f619582fc54262da7
Giovanni Squillero <squillero@polito.it>
2024-08-04 16:33:37 +0200

    feat: branch n'bound
    first working version!

--- 2024-25/golang-demos/shortest-path/main.go
+++ 2024-25/golang-demos/shortest-path/main.go
@@ -5,6 +5,8 @@
 package main
 
 import (
+	graph2 "demo-sp/graph"
+	british_museum "demo-sp/solvers"
 	"demo-sp/viz"
 	"flag"
 	"log"
@@ -26,43 +28,42 @@ func main() {
 	if *verbose {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 	}
-	slog.Debug("This is Computational Intelligence 🐹 2024/25!")
+	slog.Debug("Thisis Computational Intelligence 🐹 2024/25!")
 
 	// Create problem
-	numVertexes := 50
+	numNodes := 100
 	edgeDensity := 0.01
 
 	// all passed by reference...
-	feed := make(chan interface{}, numVertexes)
-	graph := &Graph{canvasSize: CanvasSize}
-	graph.initialize(numVertexes, edgeDensity)
+	feed := make(chan interface{}, numNodes)
+	graph := &graph2.Graph{CanvasSize: CanvasSize}
+	graph.Initialize(numNodes, edgeDensity)
 
 	go func() {
-		for _, c := range graph.nodes {
-			feed <- viz.ColoredCircle{Color: viz.ColorBabyPink, Center: c, Radius: 3}
-		}
-		for n1 := 0; n1 < numVertexes; n1 += 1 {
-			for n2 := n1 + 1; n2 < numVertexes; n2 += 1 {
-				if graph.edges[n1][n2] > 0 {
-					feed <- viz.ColoredPolyline{Color: viz.ColorBabyPink,
-						Polyline: viz.Polyline{Points: []viz.Point{graph.nodes[n1], graph.nodes[n2]}}}
+		for n1 := 0; n1 < numNodes; n1 += 1 {
+			feed <- viz.ColoredCircle{Color: viz.ColorMidnightBlue, Center: graph.Nodes[n1], Radius: 5}
+			for n2 := n1 + 1; n2 < numNodes; n2 += 1 {
+				if graph.Edges[n1][n2] > 0 {
+					feed <- viz.ColoredPolyline{Color: viz.ColorMidnightBlue,
+						Polyline: viz.Polyline{Points: []viz.Point{graph.Nodes[n1], graph.Nodes[n2]}}}
 				}
 			}
 		}
 	}()
 
-	go britishMuseum(graph, feed, 0)
-	go britishMuseum(graph, feed, 1)
-	go britishMuseum(graph, feed, 2)
-	go britishMuseum(graph, feed, 3)
+	//go func() {
+	//	for t := range viz.TagColors {
+	//		go british_museum.BranchNBound(graph, feed, int32(t))
+	//	}
+	//}()
+	go british_museum.BranchNBound(graph, feed, 0)
 
-	go func() {
-		feed <- viz.TaggedText{Tag: 0, Text: "British Museum Algorithm"}
-		feed <- viz.TaggedText{Tag: 1, Text: "British Museum Algorithm"}
-		feed <- viz.TaggedText{Tag: 2, Text: "British Museum Algorithm"}
-		feed <- viz.TaggedText{Tag: 3, Text: "British Museum Algorithm"}
-	}()
+	//go func() {
+	//	for t := 0; t < viz.MaxTags; t++ {
+	//		feed <- viz.TaggedText{Tag: int32(t), Text: "British Museum Algorithm"}
+	//	}
+	//}()
 
-	viz.Run(feed, "Zap!", CanvasSize, WindowSize)
+	viz.Run(feed, "Computational Intelligence 🐹 2024/25", CanvasSize, WindowSize)
 	//britishMuseum(graph)
 }
