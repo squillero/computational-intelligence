7655d2c24b00b033ba063b414c0e7366e9b763eb
Giovanni Squillero <squillero@polito.it>
2024-12-11 10:23:22 +0100

    Update PJ

--- 2024-25/project-work/d3584.py
+++ 2024-25/project-work/d3584.py
@@ -4,6 +4,14 @@
 
 import numpy as np
 
+# All numpy's mathematical functions can be used in formulas
+# see: https://numpy.org/doc/stable/reference/routines.math.html
+
+
+# Notez bien: No need to include f0 -- it's just an example!
+def f0(x: np.ndarray) -> np.ndarray:
+    return x[0] + np.sin(x[1]) / 5
+
 
 def f1(x: np.ndarray) -> np.ndarray: ...
 
