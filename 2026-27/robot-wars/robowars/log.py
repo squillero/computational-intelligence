266f41a8719d343ee4ea50d0f52045dfec17af3d
Giovanni Squillero <giovanni.squillero@polito.it>
2026-08-29 20:04:46 +0200

    Remove Robot Wars

--- 2026-27/robot-wars/robowars/log.py
+++
@@ -1,4 +0,0 @@
-# Copyright © 2026 Giovanni Squillero / Politecnico di Torino
-# https://github.com/squillero/computational-intelligence
-# Free under certain conditions — see the license for details.
-
